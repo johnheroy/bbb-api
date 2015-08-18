19bee06d446da49d4d8fd12817e9ffdd245b851d
John Heroy <johnheroy@gmail.com>
2015-08-17 22:58:08 -0700

    calc closest bus stop based on GPS coords

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 var request = require('request');
+var Promise = require('bluebird');
+var csv = require('csv');
+
+var stopsResolver = Promise.pending();
+var stopTimesResolver = Promise.pending();
+var STOPS = [];
+var STOP_TIMES = [];
 
 var requestSettings = {
   method: 'GET',
@@ -15,6 +22,7 @@ function getTripUpdates(tripId) {
 
       for (var i = 0; i < feed.entity.length; i++) {
         if (feed.entity[i].trip_update.trip.trip_id === tripId) {
+          // TODO: replace with a real API.
           console.log(
               'delay for trip ID',
               tripId,
@@ -27,22 +35,78 @@ function getTripUpdates(tripId) {
   });
 }
 
-// Find closest stop (or 2 stops?) for a given gps coordinate
-function getClosestStop(lat, lon) {
-  request('http://gtfs.bigbluebus.com/stop_times.txt', function(error, response, body) {
-    console.log('in here', response)
-    if (!error && response.statusCode == 200) {
-      console.log(body);
+function loadStopTimes() {
+  var stopTimesParser = csv.parse({columns: true}, function(err, data) {
+    if (!err) {
+      STOP_TIMES = data;
+      stopTimesResolver.resolve();
+      return;
     }
+
+    stopTimesResolver.reject();
   });
+  request('http://gtfs.bigbluebus.com/parsed/stop_times.txt')
+      .pipe(stopTimesParser);
 }
 
-getClosestStop(34.0361974, -118.4718219);
+function loadStops() {
+  var stopsParser = csv.parse({columns: true}, function(err, data) {
+    if (!err) {
+      STOPS = data;
+      stopsResolver.resolve();
+      // console.log(STOPS);
+      return;
+    }
+
+    stopsResolver.reject();
+  });
+  request('http://gtfs.bigbluebus.com/parsed/stops.txt')
+      .pipe(stopsParser);
+}
+
+// Find closest stop (or 2 stops?) for a given gps coordinate
+function getClosestStop(lat, lon) {
+  var minDistance = 100;
+  var closestStopIndex = -1;
+
+  for (var i = 0; i < STOPS.length; i++) {
+    var stop = STOPS[i];
+    var changeLat = lat - stop.stop_lat;
+    var changeLon = lon - stop.stop_lon;
+    var distance = Math.sqrt(changeLat * changeLat + changeLon * changeLon);
+    // console.log('distance calcd is', distance);
+    if (distance < minDistance) {
+      minDistance = distance;
+      closestStopIndex = i;
+      // console.log('closest stop now', i);
+    }
+  }
+
+  return closestStopIndex;
+}
 
 
-// Find closest stop based on GPS coordinates, get stop_id
+loadStopTimes();
+loadStops();
+Promise
+    .all([stopTimesResolver.promise, stopsResolver.promise])
+    .then(function() {
+      console.log('stops and stop times loaded!');
+
+      // Find closest stop based on GPS coordinates, get stop_id
+      var closestStopIndex = getClosestStop(34.0361974, -118.4718219);
+      var closestStopId = STOPS[closestStopIndex].stop_id;
+      console.log('closest stop is', STOPS[closestStopIndex].stop_name);
+      
+      // Get arrivals for that stop
+      var arrivals = STOP_TIMES.filter(function(element, index) {
+        return element.stop_id === closestStopId;
+      });
+      console.log(arrivals);
+    });
 
 
-// Get arrivals +/- 30 minutes for that stop
+
+
 // find delays by that trip ID
 // getTripUpdates('613771');
