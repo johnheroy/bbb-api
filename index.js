0ad22355578760ae086fad79ce74cc6eb7b62de9
John Heroy <johnheroy@gmail.com>
2015-08-18 21:38:24 -0700

    Refactor how to store static data (for now)

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+// Deps.
 var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 var request = require('request');
 var Promise = require('bluebird');
 var csv = require('csv');
 var moment = require('moment');
 
+// Resolvers to track loading the static GTFS data.
+var calendarResolver = Promise.pending();
+var routesResolver = Promise.pending();
 var stopsResolver = Promise.pending();
 var stopTimesResolver = Promise.pending();
-var STOPS = [];
-var STOP_TIMES = [];
+var tripsResolver = Promise.pending();
 
-var requestSettings = {
+// These all correspond to CSVs in the static GTFS data available.
+var STATIC_DATA = {};
+
+var realTimeRequestSettings = {
   method: 'GET',
   url: 'http://gtfs.bigbluebus.com/tripupdates.bin',
   encoding: null
@@ -17,7 +23,7 @@ var requestSettings = {
 
 // Map array of trip IDs to array of delays in seconds
 function getTripUpdates(tripId) {
-  request(requestSettings, function (error, response, body) {
+  request(realTimeRequestSettings, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
 
@@ -36,50 +42,57 @@ function getTripUpdates(tripId) {
   });
 }
 
-function loadStopTimes() {
-  var stopTimesParser = csv.parse({columns: true}, function(err, data) {
+
+/**
+ * @param {string} table String key of static data table
+ * @param {!PromiseResolver} resolver
+ * @param {string} url Remote URL of GTFS .txt file.
+ */
+function loadStaticData(table, resolver, url) {
+  var parser = csv.parse({columns: true}, function(err, data) {
     if (!err) {
-      STOP_TIMES = data;
-      stopTimesResolver.resolve();
+      STATIC_DATA[table] = data;
+      resolver.resolve();
       return;
     }
 
-    stopTimesResolver.reject();
+    resolver.reject();
   });
-  request('http://gtfs.bigbluebus.com/parsed/stop_times.txt')
-      .pipe(stopTimesParser);
+
+  request(url).pipe(parser);
 }
 
-function loadStops() {
-  var stopsParser = csv.parse({columns: true}, function(err, data) {
-    if (!err) {
-      STOPS = data;
-      stopsResolver.resolve();
-      // console.log(STOPS);
-      return;
-    }
+loadStaticData(
+    'STOP_TIMES', 
+    stopTimesResolver, 
+    'http://gtfs.bigbluebus.com/parsed/stop_times.txt');
+loadStaticData(
+    'STOPS',
+    stopsResolver,
+    'http://gtfs.bigbluebus.com/parsed/stops.txt');
 
-    stopsResolver.reject();
-  });
-  request('http://gtfs.bigbluebus.com/parsed/stops.txt')
-      .pipe(stopsParser);
+
+
+function loadCalendar() {
+
 }
 
 // Find closest stop (or 2 stops?) for a given gps coordinate
 function getClosestStop(lat, lon) {
   var minDistance = 100;
   var closestStopIndex = -1;
+  console.log('stops', STATIC_DATA.STOPS);
 
-  for (var i = 0; i < STOPS.length; i++) {
-    var stop = STOPS[i];
+  for (var i = 0; i < STATIC_DATA.STOPS.length; i++) {
+    var stop = STATIC_DATA.STOPS[i];
     var changeLat = lat - stop.stop_lat;
     var changeLon = lon - stop.stop_lon;
     var distance = Math.sqrt(changeLat * changeLat + changeLon * changeLon);
-    // console.log('distance calcd is', distance);
+    console.log('distance calcd is', distance);
     if (distance < minDistance) {
       minDistance = distance;
       closestStopIndex = i;
-      // console.log('closest stop now', i);
+      console.log('closest stop now', i);
     }
   }
 
@@ -87,8 +100,6 @@ function getClosestStop(lat, lon) {
 }
 
 
-loadStopTimes();
-loadStops();
 Promise
     .all([stopTimesResolver.promise, stopsResolver.promise])
     .then(function() {
@@ -96,11 +107,11 @@ Promise
 
       // Find closest stop based on GPS coordinates, get stop_id
       var closestStopIndex = getClosestStop(34.0361974, -118.4718219);
-      var closestStopId = STOPS[closestStopIndex].stop_id;
-      console.log('closest stop is', STOPS[closestStopIndex].stop_name);
+      var closestStopId = STATIC_DATA.STOPS[closestStopIndex].stop_id;
+      console.log('closest stop is', STATIC_DATA.STOPS[closestStopIndex].stop_name);
 
       // Get arrivals for that stop
-      var arrivals = STOP_TIMES.filter(function(element, index) {
+      var sortedArrivals = STATIC_DATA.STOP_TIMES.filter(function(element, index) {
         return element.stop_id === closestStopId;
       }).map(function(stopTime) {
         stopTime.arrival_time = moment(stopTime.arrival_time, 'HH:mm:ss');
@@ -108,7 +119,18 @@ Promise
       }).sort(function(stopTime1, stopTime2) {
         return stopTime1.arrival_time.diff(stopTime2.arrival_time);
       });
-      console.log(arrivals);
+      
+      // Now get the arrivals within say 10 minutes before now and 30 minutes
+      // from now.
+      var tenMinutesAgo = moment().subtract(10, 'minutes');
+      var inThirtyMinutes = moment().add(30, 'minutes');
+
+      var latestArrivals = sortedArrivals.filter(function(arrival) {
+        return arrival.arrival_time.isBetween(tenMinutesAgo, inThirtyMinutes);
+      });
+
+      console.log('latest arrivals!');
+      console.log(latestArrivals);
     });
 
 
