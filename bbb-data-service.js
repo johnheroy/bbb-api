e2632a352441bf74775d83591bd811948cc70a38
John Heroy <johnheroy@gmail.com>
2015-08-23 12:16:14 -0700

    Change enum for sunday to 0 from 7

--- bbb-data-service.js
+++ bbb-data-service.js
@@ -134,13 +134,13 @@ function getStopDistance(lat, lon, stop) {
  * @enum {string}
  */
 var WEEKDAYS = {
+  0: 'sunday',
   1: 'monday',
   2: 'tuesday',
   3: 'wednesday',
   4: 'thursday',
   5: 'friday',
-  6: 'saturday',
-  7: 'sunday'
+  6: 'saturday'
 };
 
 
